9b7b56937321f13769d492d620b33696efe07904
Ubuntu <ubuntu@ip-10-236-162-135.ec2.internal>
2013-07-16 18:40:13 +0000

    modified web.js to listen on 8080 and installed express

--- web.js
+++ web.js
@@ -1,27 +1,15 @@
-/*var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-}); */
-
-
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-response.send(fs.readFileSync("index.html", "utf-8"));
+    var html  = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
